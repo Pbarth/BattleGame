7ab5c581b47fe08aa10a4b33d6467b8cc2bd8686
Pierre Barthelemy <p.barthelemy@digitsole.com>
2023-05-22 10:10:26 +0200

    Initial Commit

---
+++ BattleGame/BattleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleGameApp.swift
+//  BattleGame
+//
+//  Created by Pierre BARTHELEMY on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BattleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
